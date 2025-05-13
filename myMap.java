24e610e8c97fa9967151f27f8f1b74f4a1d747ec
Mahendra1704 <mm9419284@gmail.com>
2025-05-13 09:55:16 +0530

    Add files via upload

---
+++ myMap.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class  myMap{
+  public static void main (String[] args) {
+    HashMap<String, Integer> map = new HashMap<>();
+    map.put("one",1);
+    map.put("two",2);
+    map.put("three",3);
+    map.put("four",4);
+    System.out.println(map);
+  
+  }
+}
