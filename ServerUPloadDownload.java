5bad08c81c2d5625c69d7136b25a6d4ba2433a40
Mahendra1704 <mm9419284@gmail.com>
2025-05-13 10:03:18 +0530

    Add files via upload

---
+++ ServerUPloadDownload.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class  ServerUPloadDownload{
+  public static void main (String[] args) {
+    ArrayList<Person>personList = new ArrayList<>();
+    personList.add(new Person("john","new york"));
+    personList.add(new Person("jane","London"));
+    personList.add(new Person("jack","Paris"));
+    personList.add(new Person("jill","tokyo"));
+    System.out.println(personList);
+  
+  }
+}
+class Person{
+  String name;
+  String address;
+  Person(String name, String address){
+    this.name = name;
+    this.address = address;
+  }
+}
